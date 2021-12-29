25b7b7d0f06edb330924c46fbf5e3773f3484511
Alex20010620 <58935675+Alex20010620@users.noreply.github.com>
2021-12-29 03:22:18 +0300

    Delete Lab6 directory

--- Lab6/ShapeCollector.kt
+++
@@ -1,9 +0,0 @@
-class ShapeCollector<T : Shape> {
-    private val allShapes = mutableListOf<T>()
-
-    fun add(new: T) /* ??? */ {}
-    fun addAll(new: List<T> ) /* ??? */ {}
-
-    fun getAll(): List<T> { return mutableListOf() }
-    fun getAllSorted(comparator: ShapeComparators): List<T> /* ??? */ { return mutableListOf() }
-}
